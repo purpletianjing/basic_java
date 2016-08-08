da687dc5f97417f9689b4b4a3ac85282678ba131
purpletianjing <tianjingsun@outlook.com>
2016-08-08 23:31:23 +0800

    the third exercise

---
+++ src/ExerciseThree.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by tjsun on 8/8/16.
+ */
+public class ExerciseThree {
+    public static void main(String []args) {
+        System.out.println("please input the digital for printing *'num:");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        for (int j = 0; j < i; j++) {
+            System.out.println("*");
+        }
+    }
+}
