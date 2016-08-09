60352220524809f4bf9cc29b209dafe22e78844a
purpletianjing <tianjingsun@outlook.com>
2016-08-09 09:40:38 +0800

    refactor

--- src/ExerciseFive.java
+++ src/ExerciseFive.java
@@ -5,20 +5,29 @@ import java.util.Scanner;
  */
 public class ExerciseFive {
     public static void main(String []args) {
-        int everyNum = 1;
+
         System.out.println("please input the digital for printing *'num:");
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
         i = 2 * i - 1;
         for (int j = 0; j < i; j++) {
-            for (int k = 0; k < everyNum; k++) {
-                System.out.print("*");
+            if (j > i / 2) {
+                for (int k = 0; k <= j - i / 2; k++) {
+                    System.out.print(" ");
+                }
+                for (int m = 0; m < 2 * (i - j) - 1; m++) {
+                    System.out.print("*");
+                }
+            } else {
+                for (int n = 0; n <= i / 2 - j; n++) {
+                    System.out.print(" ");
+                }
+                for (int h = 0; h < 2 * (j + 1) - 1; h++) {
+                    System.out.print("*");
+                }
             }
+
             System.out.println();
-            everyNum += 2;
-            if (j >= i / 2) {
-                everyNum -= 4;
-            }
         }
     }
 }
